e5a1e6b9cf9bb1b29a6440971b8fdbbe1dedeb73
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-30 21:02:57 -0500

    Fixing local Storage bug

--- src/JokeList.js
+++ src/JokeList.js
@@ -13,11 +13,12 @@ class JokeList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]") //localStorage if not empty, else empty array
-
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")//localStorage if not empty, else empty array
+            //clear localStorage in Browser with: window.localStorage.clear()
         }
         this.handleVote = this.handleVote.bind(this)
         this.getJokes = this.getJokes.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
      componentDidMount() {
         //Load jokes
@@ -32,20 +33,28 @@ class JokeList extends Component {
             {headers: { Accept: "application/json"}}) // wants json version instead standard html version
             jokes.push({id: uuid(), joke: response.data.joke, votes: 0})
         }      
-        this.setState({jokes: jokes})
 
-        //Saved to the window object as stirng, takes 2 params which are both strings
-        window.localStorage.setItem(
-            "jokes",
-            JSON.stringify(jokes)
-        )
+        this.setState(
+            st => ({
+                jokes: [...st.jokes, ...jokes]
+            }),
+            () =>
+              window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+            );
+        console.log(JSON.stringify(this.state.jokes))
+    }
+
+    handleClick() {
+        this.getJokes()
     }
 
     handleVote(id, delta) {
         //loops over jokes and comapres joke id to passed id and returns an object where it updates the vote value
         this.setState(st => ({
             jokes: st.jokes.map(j => j.id === id ? {...j, votes: j.votes + delta}: j)
-        }))
+        }),
+        () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes) //is run after state is set
+        ))
     }
 
   render() {
@@ -55,8 +64,8 @@ class JokeList extends Component {
           <h1 className="JokeList-title">
             <span>Dad</span> Jokes
         </h1>
-        <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-        <button className="JokeList-getmore">New Jokes</button>
+        <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="rofl-smiley" />
+        <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
       </div>
 
         <div className="JokeList-jokes">
